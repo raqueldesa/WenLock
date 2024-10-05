8c0f32533c7c3c550b785cbce292a4f02e2c96c7
Raquel de Sá <silva.raquel.de.sa@gmail.com>
2024-10-05 15:34:37 -0400

    feat: configuracao CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
+  app.enableCors({
+    origin:
+      configService.get<string>('FRONTEND_URL') || 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow cookies if needed
+  });
+
   // Acesse a variável de ambiente para a porta
   const port = configService.get<number>('BACKEND_PORT') || 3000;
   await app.listen(port);
